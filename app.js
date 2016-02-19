6fc2ccde4182b5c32b7861364225d8e5c990cc72
thiago20022002 <gebrim.goncalves@gmail.com>
2016-02-19 17:18:25 -0500

    Webcars officially deprecated Jade and replaced with EJS/HTML pages. Moved files into a view/control structure. Templating engines with EJS.

--- app.js
+++ app.js
@@ -20,7 +20,8 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+//app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -67,9 +68,10 @@ app.use(function (req, res, next) {
 if (app.get('env') === 'development') {
     app.use(function (err, req, res, next) {
         res.status(err.status || 500);
-        res.render('error', {
+        res.render('pages/error', {
             message: err.message,
-            error: err
+            error: err,
+            user : req.user 
         });
     });
 }
@@ -78,9 +80,10 @@ if (app.get('env') === 'development') {
 // no stacktraces leaked to user
 app.use(function (err, req, res, next) {
     res.status(err.status || 500);
-    res.render('error', {
+    res.render('pages/error', {
         message: err.message,
-        error: {}
+        error: {},
+        user : req.user 
     });
     next();
 });
--- routes/index.js
+++ routes/index.js
@@ -3,32 +3,24 @@ var express = require('express');
 var passport = require('passport');
 var Account = require('../models/account');
 var router = express.Router();
-
-//var WebSocketClient = require('websocket').client;
-//var wsc = new WebSocketClient();
+var path    = require("path");
 
 var router = express.Router();
 
-var isauthorized = false;
+/**
+ * GET requests, url available to let the user navagate.
+ */
+
+
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    res.render('home', {'data': ""});
-});
-
-/* GET search page. */
-router.get('/search', function (req, res, next) {
-
-    res.render('search', {title: 'Search Express'});
+    res.render('pages/index',{ user : req.user });
 });
 
 
 /* GET login page. */
 router.get('/login', function(req, res) {
-    res.render('login', { user : req.user });
-});
-
-router.post('/login', passport.authenticate('local'), function(req, res) {
-    res.redirect('/home'); //goto home-page when logged in
+    res.render('pages/login', { user : req.user });
 });
 
 /*Redirect user to root page after logout. */
@@ -41,17 +33,37 @@ router.get('/logout', function(req, res) {
 /* GET login page. */
 router.get('/home', function (req, res, next) {
     //isauthorized = true;
-    res.render('home', { user : req.user });
+    res.render('pages/index', { user : req.user });
 });
 
-router.get('/signup', function(req, res) {
-    res.render('signup', { });
+router.get('/register', function(req, res) {
+    res.render('pages/register', { user : req.user });
+});
+router.get('/ad', function(req, res) {
+    res.render('pages/ad', { user : req.user });
 });
 
-router.post('/signup', function(req, res) {
-    Account.register(new Account({ username : req.body.username }), req.body.password, function(err, account) {
+router.get('/profile', function(req, res) {
+    res.render('pages/profile', { user : req.user });
+});
+
+
+
+
+
+
+
+/**
+ * POST requests, url where client will be sending information to the server
+ */
+
+router.post('/login', passport.authenticate('local'), function(req, res) {
+    res.redirect('/home'); //goto home-page when logged in
+});
+
+router.post('/register', function(req, res) {
+    Account.register(new Account({ user : req.user ,username : req.body.username }), req.body.password, function(err, account) {
         if (err) {
-            return res.render("signup", {info: "Sorry. That username already exists. Try again."});
         }
 
         passport.authenticate('local')(req, res, function () {
@@ -60,26 +72,6 @@ router.post('/signup', function(req, res) {
     });
 });
 
-/*
-wsc.on('connect', function (connection_local) {
-    console.log('WebSocket Client Connected');
-    connection_local.on('open', function () {
-        console.log('open');
-    });
-    connection_local.on('message', function (event) {
-       // console.log('message', event.utf8Data);
-    
-        client_json_data.push(event.utf8Data);
-      //  console.log(event.utf8Data);
-      //  console.log(client_json_data);
-    });
-
-    connection_local.on('close', function () {
-        console.log('close');
-    });
-    connection = connection_local;
-});
-*/
 
 //wsc.connect('ws://localhost:8080/MavenTrends/websocket');
 module.exports = router;
