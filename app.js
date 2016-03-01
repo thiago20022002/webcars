e7a6516ef5bbcb6c8d9caa3cc842d9976e7e5e80
thiago20022002 <gebrim.goncalves@gmail.com>
2016-02-29 22:35:40 -0500

    Alpha Version 1.1.2  new mongoose

--- app.js
+++ app.js
@@ -49,10 +49,12 @@ var url_str = process.env.MONGOLAB_URI ||
     process.env.MONGOHQ_URL || configDB.url;
 // mongoose
 
+var url = "mongodb://test:test@ds019678.mlab.com:19678/heroku_b2dmx8l5";
+
 console.log(url_str);
 //var url_str = "mongodb://test:test@ds019678.mlab.com:19678/heroku_b2dmx8l5";
 
-mongoose.connect(url_str);
+mongoose.connect(url);
 console.log("passsed******************");
 
 // catch 404 and forward to error handler
