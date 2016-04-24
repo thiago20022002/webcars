368cbe476e3ddb8b861d5508024c32ed47d8d113
thiago20022002 <gebrim.goncalves@gmail.com>
2016-04-24 16:20:19 -0400

    user test 10

--- public/js/angular-app/controllers/registerCtrl.js
+++ public/js/angular-app/controllers/registerCtrl.js
@@ -73,7 +73,7 @@ function findLocationAll(zip, place, $http) {
     if (navigator.geolocation) {
         window.navigator.geolocation.getCurrentPosition(function (pos) {
             //  console.log(pos);
-            $http.get('http://maps.googleapis.com/maps/api/geocode/json?latlng=' + pos.coords.latitude + ',' + pos.coords.longitude + '&sensor=true').then(function (res) {
+            $http.get('https://maps.googleapis.com/maps/api/geocode/json?latlng=' + pos.coords.latitude + ',' + pos.coords.longitude + '&sensor=true').then(function (res) {
                 // console.log(res.data);
                 var addr = res.data.results[0];
 
